f1c1829e704e0febbb5626b901b95a03ea79e4c3
cheeeeze <nyj9349@hanmail.net>
2021-04-19 23:33:51 +0900

    update project names

--- 반응속도체크/main.js
+++ 반응속도체크/main.js
@@ -4,9 +4,9 @@
 // package.json 에서 설치한 vue 정보 가져오기
 import Vue from 'vue';
 
-import NumberBaseball from './NumberBaseball.vue';
+import responsecheck from './ResponseCheck.vue';
 
 // 그러면 내가 설치한 vue 를 통해서 Vue() 할 수 있고 
 // root 돔에 mount 하겠다는 이야기인듯
 // 즉 여기서 뷰 인스턴스를 생성함
-new Vue(NumberBaseball).$mount('#root');
+new Vue(responsecheck).$mount('#root');

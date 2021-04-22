0a7f5e094a9965d6b8c787c15904c43dbc1f860e
cheeeeze <nyj9349@hanmail.net>
2021-04-22 19:56:59 +0900

    install Loaders
    css loader, vue-style-loader

--- 반응속도체크/ResponseCheck.vue
+++ 반응속도체크/ResponseCheck.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <div id="screen">{{}}</div>
+        <div id="screen" :class="state">{{ message }}</div>
         <div>
-            <div>평균시간 : </div>
+            <div>평균시간 : {{ }} </div>
             <button @click="onRest">리셋</button>
         </div>
     </div>
@@ -11,15 +11,40 @@
 export default {
     data() {
         return {
-        }
+            result : [],
+            state : 'waiting',
+            message : '클릭해서 시작하세요.',
+        };
     },
     methods : {
         onRest() {
-            
+
         },
-    }
+    },
 };
 </script>
-<style>
+<style scoped>
+/* css 를 처리하려면
+vue-style-loader 와 css-loader 를 추가로 설치해야한다
+> npm i vue-style-loader -D (-D는 개발용을 뜻함)
+> npm i css-loader -D 
+> 그 다음 npm run build 해야함
 
+*/
+#screen {
+    width:300px;
+    height:200px;
+    text-align:center;
+    user-select: none;
+}
+#screen.waiting{
+    background-color: aqua;
+}
+#screen.ready{
+    background-color: red;
+    color:white;
+}
+#screen.new{
+    background-color: greenyellow;
+}
 </style>
